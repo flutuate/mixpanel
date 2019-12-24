6b2572164a9ae033379823a7c139c84cdcde156d
Luciano Rodrigues <luciano.rodrigues@ailos.coop.br>
2019-12-24 13:00:17 -0200

    adjusting source code

--- ios/Classes/SwiftFlutuateMixpanelPlugin.swift
+++ ios/Classes/SwiftFlutuateMixpanelPlugin.swift
@@ -10,56 +10,63 @@ public class SwiftFlutuateMixpanelPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-		switch call.method {
-			case "getInstance":
-				self.getInstance(call: call, result: result)
-				break
-			case "flush":
-				self.flush(result: result)
-				break
-			case "track":
-				self.track(call: call, result: result)
-				break
-			case "trackMap":
-				self.trackMap(call: call, result: result)
-				break
-			case "getDeviceInfo":
-				self.getDeviceInfo(result: result)
-				break
-			case "getDistinctId":
-				self.getDistinctId(result: result)
-				break
-			case "optInTracking":
-				self.optInTracking(result: result)
-				break
-			case "optOutTracking":
-				self.optOutTracking(result: result)
-				break
-			case "reset":
-				self.reset(result: result)
-				break
-			case "identify":
-				self.identify(call: call, result: result)
-				break
-			default:
-				result(FlutterMethodNotImplemented)
-		}
-		return	
+    //result("iOS " + UIDevice.current.systemVersion)
+	switch call.method {
+		case "getInstance":
+			self.getInstance(call: call, result: result)
+			break
+		case "flush":
+			self.flush(result: result)
+			break
+		case "track":
+			self.track(call: call, result: result)
+			break
+		case "trackMap":
+			self.trackMap(call: call, result: result)
+			break
+		case "getDeviceInfo":
+			self.getDeviceInfo(result: result)
+			break
+		case "getDistinctId":
+			self.getDistinctId(result: result)
+			break
+		case "optInTracking":
+			self.optInTracking(result: result)
+			break
+		case "optOutTracking":
+			self.optOutTracking(result: result)
+			break
+		case "reset":
+			self.reset(result: result)
+			break
+		case "identify":
+			self.identify(call: call, result: result)
+			break
+		default:
+			result(FlutterMethodNotImplemented)
+	}
+	return	
   }
 	
+  typealias invalidMixpanelToken = Error
+	
+//http://online.swiftplayground.run/
 //https://developer.mixpanel.com/docs/swift
 //https://api.flutter.dev/objcdoc/Classes/FlutterMethodChannel.html
 //https://stackoverflow.com/questions/50078947/how-to-implement-a-flutterplugins-method-handler-in-swift
 //https://stackoverflow.com/questions/57664458/how-to-use-passed-parameters-in-swift-setmethodcallhandler-self-methodnamere
-  private func getInstance(call: FlutterMethodCall, result: @escaping FlutterResult) {
-	let arguments = call.arguments as? Dictionary<String, Any>
-    let token = arguments["token"] as? [String: Any]
-	if let optOutTrackingDefault = arguments["optOutTrackingDefault"] as? Bool {
-		Mixpanel.initialize(token: token, optOutTrackingByDefault: optOutTrackingDefault)
-	} else {
-		Mixpanel.initialize(token: token)
-	} 
+  private func getInstance(call: FlutterMethodCall, result: @escaping FlutterResult) {  
+	if let arguments = call.arguments as? [String: Any] {
+        if let token = arguments["token"] as? [String] {
+			if let optOutTrackingDefault = arguments["optOutTrackingDefault"] as? Bool {
+				Mixpanel.initialize(token: token, optOutTrackingByDefault: optOutTrackingDefault)
+			} else {
+				Mixpanel.initialize(token: token)
+			} 
+			return
+		}
+	}
+	throw invalidToken;
   }
   
   private func flush(result: @escaping FlutterResult) {
